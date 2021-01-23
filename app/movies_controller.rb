853b5ce42a262ac5e90ebfe8c72581ea00cdf909
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-23 19:10:35 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,9 +27,7 @@ def can_be_created_in_a_block(args = "")
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  movie = Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = "1990"
+  movie = Movie.create(title: "Home Alone", release_date: "1990")
   end
 end
 
