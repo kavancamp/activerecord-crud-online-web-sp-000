616b1ac61f83c6c02596f02b55f89d1618f82fd8
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-23 19:34:15 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,8 +28,8 @@ def can_be_created_in_a_block(args = "")
   # title == "Home Alone"
   # release_date == 1990
   Movie.create do |m|
-    m.title = ""
-    m.release_date = ""
+    m.title = "The Room"
+    m.release_date = "1990"
     m.save
   end
 end
